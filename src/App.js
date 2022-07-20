64583b93110ba76dd564a39a77d1e37c1b4e6835
mbeniac <mandybeniac@gmail.com>
2022-07-20 18:06:08 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Button from "react-bootstrap/Button";
 function App() {
   return (
     <div className="navbar">
-      <h1 className="red">Marina's Shop</h1>
-      <Button variant="outline-primary">Click Here</Button>
+      <h1>Marina's Shop</h1>
+      <button className="btn btn-primary">Click Here</button>
     </div>
   );
 }
