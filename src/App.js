da7098a3949bfbd436328b89a7bb5f07f7738914
mbeniac <mandybeniac@gmail.com>
2022-07-20 19:18:03 -0500

    navbar css colors and width

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import "./App.css";
-import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="navbar">
-      <h1>Marina's Shop</h1>
-      <button className="btn btn-primary">Click Here</button>
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Marina's Shop
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About Me
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">Contact Me</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
